891ecc4b1f4e6e592dbe059d2c4e0baa7da82d0b
bao2206 <baopro2206@gmail.com>
2024-09-16 00:02:03 +0700

    Add project files.

---
+++ Dotnet.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Dotnet.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
